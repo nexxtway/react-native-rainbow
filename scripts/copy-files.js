ecc8b88ca20cc7e37fae518684e8da25392ee402
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-11-09 01:48:29 -0600

    chore: setup typescript and babel

---
+++ scripts/copy-files.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const fs = require('fs');
+const glob = require('glob');
+
+const packagePath = process.cwd();
+const buildPath = path.join(packagePath, './dist');
+const srcPath = path.join(packagePath, './src');
+
+function typescriptCopy({ from, to }) {
+    const files = glob.sync('**/*.d.ts', { cwd: from });
+    const cmds = files.map(file =>
+        fs.copyFileSync(path.resolve(from, file), path.resolve(to, file)),
+    );
+    return cmds;
+}
+
+function run() {
+    typescriptCopy({ from: srcPath, to: buildPath });
+}
+
+run();
