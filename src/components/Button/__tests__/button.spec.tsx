b48b3f7e91adf5c9e0e758c9adf72bbdf82f2e85
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-11-11 14:15:34 -0600

    fix: button test

--- src/components/Button/__tests__/button.spec.tsx
+++ src/components/Button/__tests__/button.spec.tsx
@@ -1,15 +1,16 @@
 import 'react-native';
 import React from 'react';
-import { mount } from 'enzyme';
+import TestRenderer from 'react-test-renderer';
 import Button from '../index';
 
 describe('<Button />', () => {
     it('should fire an event when press the button', () => {
         const pressMockFn = jest.fn();
-        const component = mount(
+        const component = TestRenderer.create(
             <Button label="Button Label" onPress={pressMockFn} />,
         );
-        component.simulate('click');
+        const componentInstance = component.root;
+        componentInstance.props.onPress();
         expect(pressMockFn).toHaveBeenCalledTimes(1);
     });
 });
