34d24b14793703683547156108a9b299b3d7e0d8
LeandroTorresSicilia <jtorressicilia@gmail.com>
2019-11-11 14:38:07 -0600

    chore: build with typescript

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-    presets: [
-        'module:metro-react-native-babel-preset',
-        '@babel/preset-typescript',
-    ],
-    ignore: ['src/**/*.d.ts'],
+    presets: ['module:metro-react-native-babel-preset'],
 };
