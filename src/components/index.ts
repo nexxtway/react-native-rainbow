232a796042f5cc6dbeceeb4bdda5af06e0a3b9ea
TahimiLeonBravo <tahimileon@gmail.com>
2019-11-30 23:33:03 -0600

    fix: import components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from './Button';
+export { default as ButtonIcon } from './ButtonIcon';
 export { default as Card } from './Card';
 export { default as Input } from './Input';
+export { default as InputCode } from './InputCode';
--- storybook/stories/input.stories.js
+++ storybook/stories/input.stories.js
@@ -13,11 +13,7 @@ storiesOf('Input', module)
         <>
             <Input style={styles} />
             <Input style={styles} label="Label" placeholder="Type here" />
-            <Input
-                style={styles}
-                label="Disabled Input"
-                disabled
-            />
+            <Input style={styles} label="Disabled Input" disabled />
             <Input
                 style={styles}
                 label="Label"
