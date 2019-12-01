cade8a6e44c0fa13e792b1ad377b29b1487bc9fe
TahimiLeonBravo <tahimileon@gmail.com>
2019-11-30 20:41:41 -0600

    styles: fix input code

--- src/components/InputCode/styled/container.ts
+++ src/components/InputCode/styled/container.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components/native';
 const Container = styled.View<{}>`
     display: flex;
     flex-direction: row;
+    justify-content: center;
+    align-items: flex-start;
+    flex-shrink: 0;
 `;
 
 export default Container;
