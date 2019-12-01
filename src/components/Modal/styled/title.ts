b8ddbd02e8dcf5299273fb3f44bf1c22657228fd
TahimiLeonBravo <tahimileon@gmail.com>
2019-12-01 10:40:24 -0600

    styles: modal

---
+++ src/components/Modal/styled/title.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import { COLOR_DARK_1 } from '../../../styles/colors';
+
+const Title = styled.Text<{}>`
+    font-size: 18;
+    color: ${COLOR_DARK_1};
+    margin: 24px 40px;
+    text-align: center;
+`;
+
+export default Title;
--- storybook/stories/modal.stories.js
+++ storybook/stories/modal.stories.js
@@ -1,8 +1,16 @@
 import React, { createElement, useState } from 'react';
 import { storiesOf } from '@storybook/react-native';
 import CenterView from './CenterView';
+import { Text } from 'react-native';
 import Button from '../../src/components/Button';
 import Modal from '../../src/components/Modal';
+
+const styles = {
+    margin: 24,
+    fontSize: 14,
+    textAlign: 'center',
+    lineHeight: 20,
+};
 
 storiesOf('Modal', module)
     .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
@@ -20,7 +28,19 @@ storiesOf('Modal', module)
                         isOpen={isOpen}
                         title="Modal Title"
                         onRequestClose={() => setIsOpen(false)}
-                    />
+                    >
+                        <Text style={styles}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris nisi ut aliquip
+                            ex ea commodo consequat. Duis aute irure dolor in
+                            reprehenderit in voluptate velit esse cillum dolore
+                            eu fugiat nulla pariatur. Excepteur sint occaecat
+                            cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum.
+                        </Text>
+                    </Modal>
                 </>
             );
         }),
