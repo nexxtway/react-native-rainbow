79eb2253481a6da7e7c0c447331c4e0ce0b11161
TahimiLeonBravo <tahimileon@gmail.com>
2019-12-08 11:41:04 -0600

    chore: add InputPhone to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as ButtonIcon } from './ButtonIcon';
 export { default as Card } from './Card';
 export { default as Input } from './Input';
 export { default as InputCode } from './InputCode';
+export { default as InputPhone } from './InputPhone';
