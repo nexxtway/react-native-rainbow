6e69788fc98d25b5c95addf11959a072a6a08134
Ramon Chancay <ramon.chancay@gmail.com>
2019-12-09 15:18:07 -0500

    story bagde node

--- storybook/stories/badge.stories.js
+++ storybook/stories/badge.stories.js
@@ -3,6 +3,9 @@ import { storiesOf } from '@storybook/react-native';
 
 import CenterView from './CenterView';
 import Badge from '../../src/components/Badge';
+import { Text } from 'react-native';
+
+const BadgeNode = () => <Text>Hi</Text>;
 
 storiesOf('Badge', module)
     .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
@@ -13,4 +16,5 @@ storiesOf('Badge', module)
     .add('Brand', () => <Badge label="BADGE BRAND" variant="brand" />)
     .add('Outline Brand', () => (
         <Badge label="BADGE OUTLINE BRAND" variant="outline-brand" />
-    ));
+    ))
+    .add('React node', () => <Badge label={<BadgeNode />} />);
