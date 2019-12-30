90e3822fb2180e53645b0a2ccf0f522e9c4eed16
TahimiLeonBravo <tahimileon@gmail.com>
2019-12-29 18:41:55 -0600

    fix: import Modal

--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export { default as InputPhone } from './InputPhone';
 export { default as Avatar } from './Avatar';
 export { default as Badge } from './Badge';
 export { default as ProgressCircular } from './ProgressCircular';
+export { default as Modal } from './Modal';
