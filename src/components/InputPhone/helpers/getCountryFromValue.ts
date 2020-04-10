9f2a5e9309e276f4956376610437e0a92f8f5cf8
Ramon Chancay <ramon.chancay@gmail.com>
2020-04-10 09:15:45 -0500

    fix default Country on the InputPhone close #50

--- src/components/InputPhone/helpers/getCountryFromValue.ts
+++ src/components/InputPhone/helpers/getCountryFromValue.ts
@@ -1,5 +1,13 @@
+import * as RNLocalize from 'react-native-localize';
 import COUNTRIES from '../countyPickerModal/countries';
 
 export default function getCountryFromValue(isoCode?: string) {
+    if (isoCode === '') {
+        const currentCountry = RNLocalize.getCountry();
+        return COUNTRIES.find(
+            country => country.isoCode === currentCountry.toLowerCase(),
+        );
+    }
+
     return COUNTRIES.find(country => country.isoCode === isoCode);
 }
