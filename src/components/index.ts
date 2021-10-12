8c326f8c906ff9b920ae64264f21ee5ea70e66f2
LeandroTorresSicilia <jtorressicilia@gmail.com>
2021-10-11 22:42:43 -0400

    fix: lint error

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Application } from './Application';
+export { default as Avatar } from './Avatar';
 export { default as AvatarGroup } from './AvatarGroup';
 export { default as Badge } from './Badge';
 export { default as Button } from './Button';
