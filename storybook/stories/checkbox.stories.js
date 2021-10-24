ca2c75ccf0b7ce755d97da75849f425125fcf756
LeandroTorresSicilia <jtorressicilia@gmail.com>
2021-10-24 17:12:31 -0400

    feat: add checkbox component

---
+++ storybook/stories/checkbox.stories.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react-native';
+import ThemeWrapper from './ThemeWrapper';
+import CenterView from './CenterView';
+import Checkbox from '../../src/components/Checkbox';
+
+const styles = {
+    marginBottom: 16,
+};
+
+const ControlledCheckbox = props => {
+    const [value, setValue] = useState(props.value);
+    return <Checkbox {...props} value={value} onChange={setValue} />;
+};
+
+storiesOf('Checkbox', module)
+    .addDecorator(getStory => (
+        <ThemeWrapper>
+            <CenterView>{getStory()}</CenterView>
+        </ThemeWrapper>
+    ))
+    .add('basic', () => (
+        <>
+            <ControlledCheckbox style={styles} label="Label" value />
+            <ControlledCheckbox style={styles} label="Label" error="This is a required field" />
+            <ControlledCheckbox style={styles} label="Disabled Checkbox" disabled />
+            <ControlledCheckbox style={styles} label="Disabled Checkbox" value disabled />
+        </>
+    ));
