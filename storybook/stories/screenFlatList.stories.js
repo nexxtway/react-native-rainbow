5b8e615a4faf1b7ec3f798e5c6d53dbf7ee9a3b3
David Rodriguez Feito <drodriguezfeito@gmail.com>
2022-03-03 22:57:23 -0500

    fix: load more items per page on examples

--- storybook/stories/screenFlatList.stories.js
+++ storybook/stories/screenFlatList.stories.js
@@ -26,14 +26,15 @@ const Item = ({ item }) => {
     );
 };
 
+const perPage = 20;
 const dataSource = ({ previousResponse, items }) => {
     return new Promise((resolve, reject) => {
         const startIndex = !previousResponse ? 1 : previousResponse.startIndex;
-        const newItems = Array.from(Array(10).keys()).map(i => `Item ${i + startIndex}`);
+        const newItems = Array.from(Array(perPage).keys()).map(i => `Item ${i + startIndex}`);
         setTimeout(() => {
             resolve({
                 items: newItems,
-                startIndex: startIndex + 10,
+                startIndex: startIndex + perPage,
             });
         }, 3000);
     });
